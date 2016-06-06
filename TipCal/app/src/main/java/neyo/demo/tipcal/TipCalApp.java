3642b64a4a6b99aa34d2ff9c7706e5e37f5712b5
neyogiry <yrigoyen.fernando@gmail.com>
2016-06-05 20:20:43 -0500

    Third commit

---
+++ TipCal/app/src/main/java/neyo/demo/tipcal/TipCalApp.java
@@ -0,0 +1,14 @@
+package neyo.demo.tipcal;
+
+import android.app.Application;
+
+/**
+ * Created by Jose Fernando on 05/06/2016.
+ */
+public class TipCalApp extends Application {
+    private final static String ABOUT_URL = "https://github.com/neyogiry";
+
+    public static String getAboutUrl() {
+        return ABOUT_URL;
+    }
+}
