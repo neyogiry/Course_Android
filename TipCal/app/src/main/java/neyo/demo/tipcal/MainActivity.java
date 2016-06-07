0d33110afea2d2c6763375f08e4981eead391741
neyogiry <yrigoyen.fernando@gmail.com>
2016-06-07 12:15:50 -0500

    Fourth commit

--- TipCal/app/src/main/java/neyo/demo/tipcal/MainActivity.java
+++ TipCal/app/src/main/java/neyo/demo/tipcal/MainActivity.java
@@ -1,18 +1,46 @@
 package neyo.demo.tipcal;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.input.InputManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.inputBill) EditText inputBill;
+
+    @BindView(R.id.btnSubmit) Button btnSubmit;
+
+    @BindView(R.id.inputPercentage) EditText inputPercentage;
+
+    @BindView(R.id.btnIncrease) Button btnIncrease;
+
+    @BindView(R.id.btnDecrease) Button btnDecrease;
+
+    @BindView(R.id.btnClear) Button btnClear;
+
+    @BindView(R.id.txtTip) EditText txtTip;
+
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_PERCENTAGE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -29,12 +57,29 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.btnSubmit)
+    public void handleClickSubmir(){
+        Log.e(getLocalClassName(), "Click en Submit");
+        hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        try{
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (NullPointerException npe){
+            Log.e(getLocalClassName(), Log.getStackTraceString(npe));
+        }
+    }
+
     private void about() {
         TipCalApp app = (TipCalApp)getApplication();
         String strUrl = app.getAboutUrl();
 
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(strUrl);
+        intent.setData(Uri.parse(strUrl));
         startActivity(intent);
     }
 }
